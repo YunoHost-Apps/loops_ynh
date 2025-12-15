ec86846020c1cb25622cb7b5256f8a04d9758622
Severin <savewish@icloud.com>
2025-12-15 11:37:46 +0100

    Fetch newest Redis Bloom Version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,10 +7,9 @@
 timezone=$(timedatectl show --value --property=Timezone)
 
 # REDIS BLOOM INSTALL HELPER 
-
-REDIS_MODULE_DIR="/etc/redis/modules"
-REDISBLOOM_SO="$REDIS_MODULE_DIR/redisbloom.so"
-REDIS_CONF="/etc/redis/redis.conf"
-
-REDISBLOOM_VERSION="v2.8.17"
-REDISBLOOM_URL="https://github.com/RedisBloom/RedisBloom/archive/refs/tags/v2.8.17.tar.gz"
+# Fetch latest RedisBloom release tag from GitHub
+get_latest_redisbloom_version() {
+    curl -s https://api.github.com/repos/RedisBloom/RedisBloom/releases/latest \
+        | grep '"tag_name"' \
+        | cut -d '"' -f 4
+}
