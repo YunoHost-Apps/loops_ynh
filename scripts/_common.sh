1b4de9f31514103fc02eeb1335b3fb42a8566a54
Severin <savewish@icloud.com>
2025-12-15 10:39:25 +0100

    Fix Redis Bloom to v2.8.17

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,4 +13,4 @@ REDISBLOOM_SO="$REDIS_MODULE_DIR/redisbloom.so"
 REDIS_CONF="/etc/redis/redis.conf"
 
 REDISBLOOM_VERSION="v2.8.17"
-REDISBLOOM_URL="https://github.com/RedisBloom/RedisBloom/releases/download/${REDISBLOOM_VERSION}/redisbloom-${REDISBLOOM_VERSION}.tar.gz"
+REDISBLOOM_URL="https://github.com/RedisBloom/RedisBloom/archive/refs/tags/v2.8.17.tar.gz"
