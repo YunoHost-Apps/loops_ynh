061a79005735c802c11280c69f74a4b4f0734441
Severin <savewish@icloud.com>
2025-12-16 09:20:39 +0100

    Update common helper with RedisBloom locations and version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,12 @@
 #=================================================
 
 timezone=$(timedatectl show --value --property=Timezone)
+
+# REDIS BLOOM INSTALL HELPERS
+
+REDIS_MODULE_DIR="/etc/redis/modules"
+REDISBLOOM_SO="$REDIS_MODULE_DIR/redisbloom.so"
+REDIS_CONF="/etc/redis/redis.conf"
+
+REDISBLOOM_VERSION="v2.8.17"
+REDISBLOOM_REPO="https://github.com/RedisBloom/RedisBloom.git"
